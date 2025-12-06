cd065932e8bb1dd091e0c71516e79a81d0c3a59f
teuaraujoo <empresarial.mateus25@gmail.com>
2025-12-06 12:31:21 -0300

    pontos turisticos section

---
+++ src/script/script.js
@@ -0,0 +1,16 @@
+// 2. Animação de entrada para a seção de pontos turísticos
+const pontos = gsap.utils.toArray('#pontos > div');
+
+    pontos.forEach(ponto => {
+        gsap.from(ponto, {
+            opacity: 0,
+            y: 100, // Começa 100px abaixo da posição final
+            duration: 0.8,
+            ease: 'power2.out',
+            scrollTrigger: {
+                trigger: ponto,
+                start: 'top 85%', // A animação começa quando o topo do elemento está a 85% da altura da tela
+                toggleActions: 'play none none none', // A animação executa uma vez e não reverte
+            }
+        });
+    });
